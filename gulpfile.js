e0a1168775b9393a294ce2d812a0e3a0b0bf3a55
Varya Stepanova <mail@varya.me>
2015-02-19 10:11:35 +0200

    Simple live-reloaded server with working version

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp');
+ 
+var server = require('gulp-express');
+ 
+var options = require('./config');
+ 
+// Shortcats
+var sourcePath = options.sourcePath;
+var outputPath = options.outputPath;
+var appPath = options.appPath;
+ 
+gulp.task('build:app', [
+    'build:html'
+    // TODO: Add tasks about CSS and JS
+    ]);
+ 
+gulp.task('build:html', function() {
+    // Copy HTML
+    return gulp.src(sourcePath + '/**/*.html')
+        .pipe(gulp.dest(outputPath));
+ 
+});
+ 
+gulp.task('serve', ['build:app'], function() {
+    server.run({
+        file: appPath
+    });
+ 
+    gulp.watch(sourcePath + '**/*.html', ['build:html']);
+ 
+    // liveReload when changes appear
+    // TODO: can be more specific in the future
+    gulp.watch(outputPath + '/**', server.notify);
+});
+ 
+gulp.task('default', ['serve']);
