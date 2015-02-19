a77744b915cc57668dbb5af069723fddc7bdffec
Varya Stepanova <mail@varya.me>
2015-02-19 18:25:52 +0200

    Codestyle

--- config.js
+++ config.js
@@ -4,13 +4,13 @@
 
     var options = {
         // Application options
-        appPath: './lib/app/app.js',
+        appPath: "./lib/app/app.js",
         port: 8080,
         livereloadPort: 35729,
 
         // Build options
-        sourcePath: './src',
-        outputPath: './output'
+        sourcePath: "./src",
+        outputPath: "./output"
     }
 
     module.exports = options;
