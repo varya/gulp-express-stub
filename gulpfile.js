f07fe7708717983a2007bee76f4cca984274e163
Varya Stepanova <mail@varya.me>
2015-02-19 18:26:10 +0200

    Watch over JavaScript and CSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,49 @@
-var gulp = require('gulp');
- 
-var server = require('gulp-express');
- 
-var options = require('./config');
- 
+var gulp = require("gulp");
+var server = require("gulp-express");
+
+var options = require("./config");
+
 // Shortcats
 var sourcePath = options.sourcePath;
 var outputPath = options.outputPath;
 var appPath = options.appPath;
- 
-gulp.task('build:app', [
-    'build:html'
-    // TODO: Add tasks about CSS and JS
+
+gulp.task("build:app", [
+    "build:html",
+    "build:css",
+    "build:js"
     ]);
- 
-gulp.task('build:html', function() {
-    // Copy HTML
-    return gulp.src(sourcePath + '/**/*.html')
+
+gulp.task("build:css", function() {
+    return gulp.src(sourcePath + "/*.css")
+        // Add your building for CSS here
         .pipe(gulp.dest(outputPath));
- 
 });
- 
-gulp.task('serve', ['build:app'], function() {
+
+gulp.task("build:js", function() {
+    return gulp.src(sourcePath + "/*.js")
+        // Add your building for JS here
+        .pipe(gulp.dest(outputPath));
+});
+
+gulp.task("build:html", function() {
+    // Copy HTML
+    return gulp.src(sourcePath + "/**/*.html")
+        .pipe(gulp.dest(outputPath));
+
+});
+
+gulp.task("serve", ["build:app"], function() {
     server.run([
         appPath
     ]);
- 
-    gulp.watch(sourcePath + '**/*.html', ['build:html']);
- 
+
+    gulp.watch(sourcePath + "/**/*.html", ["build:html"]);
+    gulp.watch(sourcePath + "/**/*.css", ["build:css"]);
+    gulp.watch(sourcePath + "/**/*.js", ["build:js"]);
+
     // liveReload when changes appear
-    // TODO: can be more specific in the future
     gulp.watch(outputPath + '/**', server.notify);
 });
- 
-gulp.task('default', ['serve']);
+
+gulp.task("default", ["serve"]);
